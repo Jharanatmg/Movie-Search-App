f707b76ae48ee08bc4a9da2a03b94096f9b8f581
Jharanatmg <jharanatmg22@gmail.com>
2023-07-12 15:16:03 -0700

    implement searchbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,31 @@ import Registerpage from "./pages/Registerpage";
 import Homepage from "./pages/Homepage";
 import SearchBar from "./components/SearchBar";
 import Navbar from "./components/navbar";
-import bimg from './assets/movie-9pvmdtvz4cb0xl37.jpg'
+import bimg from "./assets/movie-9pvmdtvz4cb0xl37.jpg";
 function App() {
-  return <>
-  <div className="backgroundImg  min-h-screen" style={{ backgroundImage: `url(${bimg})`, backgroundPosition:'center', backgroundRepeat:'no-repeat', backgroundSize:'cover'}}>
-  <div className="min-h-screen w-full bg-black bg-opacity-80">
-  <Navbar/>
-   <SearchBar/>
-  <Routes>
-     <Route path="/" element={<Homepage/>}></Route>
-    <Route path="Registerpage" element={<Registerpage/>}></Route>
-    <Route path="Loginpage" element={<Loginpage/>}></Route>
-  </Routes>
-  </div>
-  </div>
-  </>
+  return (
+    <>
+      <div
+        className="backgroundImg  min-h-screen"
+        style={{
+          backgroundImage: `url(${bimg})`,
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="min-h-screen w-full bg-black bg-opacity-80">
+          <Navbar />
+          <SearchBar />
+          <Routes>
+            <Route path="/" element={<Homepage />}></Route>
+            <Route path="Registerpage" element={<Registerpage />}></Route>
+            <Route path="Loginpage" element={<Loginpage />}></Route>
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
