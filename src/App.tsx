f8436a317cde8e675ef5063030a9decf6a726bbc
Jharanatmg <jharanatmg22@gmail.com>
2023-07-11 21:15:15 -0700

    navbar link to pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import Loginpage from "./pages/Loginpage";
 import Registerpage from "./pages/Registerpage";
 import Homepage from "./pages/Homepage";
+import Navbar from "./components/navbar";
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Registerpage />}></Route>
+        <Route path="Registerpage" element={<Registerpage />}></Route>
         <Route path="Loginpage" element={<Loginpage />}></Route>
       </Routes>
     </>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,15 +1,25 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="m-1 h-12 bg-black text-white flex justify-center items-center">
-      <div className=" flex inline gap-5 items-center text-xs">
+    <div className=" flex justify-center items-center">
+      <div className="w-2/3 mt-2 h-12 bg-black flex inline gap-5 pl-4 items-center text-white text-xs">
         <a> FILMS </a>
         <a> SERIAL </a>
         <a> GAMES </a>
         <a> CINEMA REPERTORY </a>
         <a> TV PROGRAM </a>
         <a> MAGAZINE </a>
+      </div>
+      <div className="mt-2 h-12 bg-blue-500 divide-x-2 flex items-center text-sm">
+        <a className="p-2">
+          {" "}
+          <Link to="/Loginpage"> LOGIN </Link>
+        </a>
+        <a className="p-2">
+          {" "}
+          <Link to="/Registerpage">REGISTER </Link>{" "}
+        </a>
       </div>
     </div>
   );
