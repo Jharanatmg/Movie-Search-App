c037e3184b28387b94c26e083aeb03f3cbedf428
Kabina Thapa <thapakabina28@gmail.com>
2023-07-12 21:33:51 -0400

    update on searchBar

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import Loginpage from "./pages/Loginpage";
 import Registerpage from "./pages/Registerpage";
 import Homepage from "./pages/Homepage";
-import SearchBar from "./components/SearchBar";
+import SearchBar from "./components/searchbar";
 import Navbar from "./components/navbar";
 import bimg from "./assets/movie-9pvmdtvz4cb0xl37.jpg";
 function App() {
   return <>
   
-  <div className="backgroundImg min-h-screen" style={{ backgroundImage: `url(${bimg})`, backgroundPosition:'center', backgroundRepeat:'no-repeat', backgroundSize:'cover'}}>
+  <div className="backgroundImg h-screen" style={{ backgroundImage: `url(${bimg})`, backgroundPosition:'center', backgroundRepeat:'no-repeat', backgroundSize:'cover'}}>
   <div className="h-screen  bg-black bg-opacity-80">
   <Navbar/>
    <SearchBar/>
